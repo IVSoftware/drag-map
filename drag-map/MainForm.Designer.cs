c215252074862affa73558a18e509066abc5f34c
Tom Gregor <tgregor@ivsoftware.com>
2022-12-11 13:23:28 -0700

    Add project files.

---
+++ drag-map/MainForm.Designer.cs
@@ -0,0 +1,63 @@
+﻿
+namespace drag_map
+{
+    partial class MainForm
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.countryMapImage = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // countryMapImage
+            // 
+            this.countryMapImage.BackColor = System.Drawing.Color.PaleTurquoise;
+            this.countryMapImage.Location = new System.Drawing.Point(27, 39);
+            this.countryMapImage.Name = "countryMapImage";
+            this.countryMapImage.Size = new System.Drawing.Size(100, 100);
+            this.countryMapImage.TabIndex = 0;
+            this.countryMapImage.Text = "Map";
+            this.countryMapImage.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(10F, 25F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.countryMapImage);
+            this.Name = "MainForm";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "MainForm";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label countryMapImage;
+    }
+}
+
---
+++ drag-map/MainForm.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace drag_map
+{
+    public partial class MainForm : Form
+    {
+        public MainForm()
+        {
+            InitializeComponent();
+            countryMapImage.MouseUp += onMapMouseUp;
+            countryMapImage.MouseMove += onMapMouseMove;
+            countryMapImage.MouseDown += onMapMouseDown;
+        }
+
+        Point
+            _mouseDownScreen = new Point(),
+            _mouseDelta = new Point(),
+            _controlDownPoint = new Point();
+        private void onMapMouseDown(object sender, MouseEventArgs e)
+        {
+            if (sender is Control control)
+            {                
+                _mouseDownScreen = control.PointToScreen(e.Location);
+                Text = $"{_mouseDownScreen}";
+                _controlDownPoint = countryMapImage.Location;
+            }
+        }
+
+        private void onMapMouseMove(object sender, MouseEventArgs e)
+        {
+            if (MouseButtons.Equals(MouseButtons.Left))
+            {
+                if (sender is Control control)
+                {
+                    var screen = control.PointToScreen(e.Location);
+                    _mouseDelta = new Point(screen.X - _mouseDownScreen.X, screen.Y - _mouseDownScreen.Y);
+                    Text = $"{_mouseDownScreen} {screen} {_mouseDelta}";
+                    var newControlLocation = new Point(_controlDownPoint.X + _mouseDelta.X, _controlDownPoint.Y + _mouseDelta.Y);
+                    if(!countryMapImage.Location.Equals(newControlLocation))
+                    {
+                        countryMapImage.Location = newControlLocation;
+                    }
+                }
+            }
+        }
+
+        private void onMapMouseUp(object sender, MouseEventArgs e)
+        {
+        }
+    }
+}
